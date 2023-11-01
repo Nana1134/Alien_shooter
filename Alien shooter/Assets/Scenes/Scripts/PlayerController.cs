f98ef0c57b85d824c338f37afde3ce0d2a521969
Nana1134 <pudovaaa1134@gmail.com>
2023-11-02 04:21:55 +0500

    Add shooting
    Add skybox. Implemented a player controller and the mechanics of shooting with raycast.

---
+++ Alien shooter/Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody rb;
+	public float rotationSpeed = 300f;
+	// Start is called before the first frame update
+	void Start()
+    {
+		rb = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		float rotationY = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+		float rotationX = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+		transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+	}
+}
