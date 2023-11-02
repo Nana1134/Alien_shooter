8c9afe8d9584f846d62100780eaac28969fd5964
AnnaZ4 <anna.zzamyslova@gmail.com>
2023-11-02 15:31:45 +0500

    Appearance and movement of the target
    In the created world, adding mechanics for the appearance and movement of the target

---
+++ Alien shooter/Assets/Scenes/Scripts/Comet_Movement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comet_Movement : MonoBehaviour
+{
+    public Vector3 targetPosition;
+    public float smoothTime = 0.5f;
+    public float speed = 10;
+    Vector3 velocity;
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, speed);
+    }
+}
