9fb75ba794b938070b30b26fa99ed3f758eb3643
Nana1134 <pudovaaa1134@gmail.com>
2023-11-30 04:50:51 +0500

    Start lvl UI

--- Alien shooter/Assets/Scenes/Scripts/Shooting.cs
+++ Alien shooter/Assets/Scenes/Scripts/Shooting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -7,7 +8,10 @@ public class NewBehaviourScript : MonoBehaviour
 	public GameObject BulletPrefab;
 	public Camera mainCamera;
 	public Transform spawnBullet;
+	[SerializeField] TextMeshProUGUI textSnake;
+
 	public float BulletVelocity = 180f;
+	public int countShoot = 0;
 	// Start is called before the first frame update
 	void Start()
     {
@@ -20,6 +24,7 @@ public class NewBehaviourScript : MonoBehaviour
 		if (Input.GetMouseButtonDown(0))
 		{
 			Shoot();
+			countShoot++;
 		}
 	}
 	private void Shoot()
